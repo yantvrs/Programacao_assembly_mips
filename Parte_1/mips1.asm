02fd40d9d2f9d5e8370f84f93b67c6aee22b6239
Yan Tavares <yantavares2002@gmail.com>
2023-07-01 18:51:17 -0300

    Parte 1 da atividade de arquitetura de computadores pronta

--- Parte_1/mips1.asm
+++ Parte_1/mips1.asm
@@ -1,53 +1,57 @@
-.data
-
-vector: .word 9,5,7,5,3,4,0,2,6,4,2,5,4,1,2,1,6,2,2,3,6,3,0,0,7,8,3,4,5,4,0,5,2,9,8,7
-histogram: .space 50
-new_line: .asciiz "\n"
-comma: .asciiz ","
+# Atividade assembly mips 1 
+# Discente: Vinícius Yan Tavares Nascimento
+# Matrícula: 20200069226
 
 
-.text
+.data	# Inicia da seção de dados do programa
+
+vector: .word 9,5,7,5,3,4,0,2,6,4,2,5,4,1,2,1,6,2,2,3,6,3,0,0,7,8,3,4,5,4,0,5,2,9,8,7	# Declara a variável "vector" como um vetor de palavras( inteiros de 32 bits)
+histogram: .space 50 # Declara a variável "histogram" como uma área de espaço na memória de 50 bytes.
+
+# ".asciliz" declara uma sequência de caracteres na memória 
+new_line: .asciiz "\n"	# Nova linha
+comma: .asciiz ","	# Vírgula
+
+
+.text	# Declara início da seção de códigos do programa
 .globl main
 
 main:
-    li $s0, 0         # contador do loop
-    la $s1, histogram   # endereço do histograma
-    li $s2, 36        # tamanho do sinal
-    la $s3, vector     # endereço do sinal
+    li $s0, 0          	# Inicializa o registrador $s0 (contador do loop) com o valor 0
+    la $s1, histogram	# Carrega o endereço da variável "histogram" para o registrador $s1
+    li $s2, 36        	# Inicializa o registrador $s2 com o valor 36 (tamanho do sinal)
+    la $s3, vector     # Carrega o endereço da variável "vector" para o registrador $s3
 
 histogramFunc:
-    lw $s4, ($s3)     # carrega o valor do sinal
-    sll $s4, $s4, 2   # multiplica o valor por 4
-    add $s5, $s1, $s4   # calcula o endereço correspondente no histograma
-    lw $t0, ($s5)     # carrega o valor atual do histograma
-    addiu $t0, $t0, 1   # incrementa o valor
-    sw $t0, ($s5)     # armazena o novo valor no histograma
-    addiu $s3, $s3, 4   # avança para o próximo valor do sinal
-    addiu $s0, $s0, 1   # incrementa o contador
-    bne $s0, $s2, histogramFunc   # verifica se o contador chegou ao fim
+    lw $s4, ($s3)     			# Carrega o valor do sinal para o registrador $s4
+    sll $s4, $s4, 2   			# Multiplica o valor por 4 (shift left logical)
+    add $s5, $s1, $s4   		# Calcula o endereço correspondente no histograma
+    lw $t0, ($s5)     			# Carrega o valor atual do histograma para o registrador $t0
+    addiu $t0, $t0, 1   		# Incrementa o valor do histograma em 1
+    sw $t0, ($s5)     			# Armazena o novo valor no histograma
+    addiu $s3, $s3, 4   		# Avança para o próximo valor do sinal
+    addiu $s0, $s0, 1   		# Incrementa o contador do loop em 1
+    bne $s0, $s2, histogramFunc   	# Verifica se o contador chegou ao fim e, se não, salta para "histogramFunc"
+    li $s0, 0         			# Reinicia o contador do loop com o valor 0
+    li $s2, 10       			# Define o número de elementos a serem mostrados
 
-    li $s0, 0         # reinicia o contador
-    li $s2, 10        # número de elementos para mostrar
 
-mostrar:
-    li $v0, 1         # código da syscall para impressão de um inteiro
-    lw $a0, ($s1)     # carrega o valor do histograma
-    syscall           # imprime o valor
+show:
+    li $v0, 1         		# Carrega o código da syscall para impressão de um inteiro para o registrador $v0
+    lw $a0, ($s1)     		# Carrega o valor do histograma para o registrador $a0
+    syscall           		# Chama a syscall para imprimir o valor do histograma
+    addiu $s1, $s1, 4   	# Avança para o próximo valor do histograma
+    addiu $s0, $s0, 1   	# Incrementa o contador do loop em 1
+    li $v0, 4         		# Carrega o código da syscall para impressão de uma string para o registrador $v0
+    beq $s0, $s2, show_line  	# Verifica se todos os elementos foram mostrados e, se sim, salta para "show_line"
+    la $a0, comma   		# Carrega o endereço da vírgula para o registrador $a0
+    syscall           		# Chama a syscall para imprimir a vírgula
+    j show         		# Salta para o início do loop em "show"
 
-    addiu $s1, $s1, 4   # avança para o próximo valor do histograma
-    addiu $s0, $s0, 1   # incrementa o contador
 
-    li $v0, 4         # código da syscall para impressão de uma string
-    beq $s0, $s2, imprimir_linha   # verifica se todos os elementos foram mostrados
-    la $a0, comma   # carrega o endereço da vírgula
-    syscall           # imprime a vírgula
-
-    j mostrar         # volta para o início do loop
-
-imprimir_linha:
-    li $v0, 4         # código da syscall para impressão de uma string
-    la $a0, new_line   # carrega o endereço da nova linha
-    syscall           # imprime a nova linha
-
-    li $v0, 10        # código da syscall para finalizar o programa
-    syscall           # finaliza o programa
+show_line:
+    li $v0, 4         # Carrega o código da syscall para impressão de uma string para o registrador $v0
+    la $a0, new_line  # Carrega o endereço da nova linha para o registrador $a0
+    syscall           # Chama a syscall para imprimir a nova linha
+    li $v0, 10        # Carrega o código da syscall para finalizar o programa para o registrador $v0
+    syscall           # Chama a syscall para finalizar o programa
